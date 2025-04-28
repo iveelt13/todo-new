8f52fc7161dacf8aa5090ce7b2855917ced25ca4
24LP9923 24LP9923 <iveelt6655@gmail.com>
2025-04-28 12:25:07 +0800

    project

---
+++ src/components/task/TaskList.jsx
@@ -0,0 +1,20 @@
+import { TaskRow } from "./TaskRow";
+export const TaskList = ({
+  taskListValue,
+  onToggleTaskStatus,
+  onRemoveTask,
+}) => {
+  return (
+    <div>
+      {" "}
+      {taskListValue.map((task, index) => (
+        <TaskRow
+          key={index}
+          onRemoveTask={onRemoveTask}
+          onToggleTaskStatus={onToggleTaskStatus}
+          task={task}
+        />
+      ))}{" "}
+    </div>
+  );
+};
